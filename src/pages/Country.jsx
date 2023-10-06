d86c51d9687aa8cae644113e049903f8d83cac60
ViktoriaReminna <121427776+ViktoriaReminna@users.noreply.github.com>
2023-10-06 22:26:17 +0300

    all

--- src/pages/Country.jsx
+++ src/pages/Country.jsx
@@ -1,10 +1,35 @@
-import { Section, Container, CountryInfo, Loader } from 'components';
+import { Section, Container, CountryInfo, Loader, GoBackBtn } from 'components';
+import { useEffect, useState } from 'react';
+import { useLocation, useParams } from 'react-router-dom';
+import { fetchCountry } from 'service/country-service';
 
 export const Country = () => {
+  const [findCountry, setFindCountry] = useState({});
+  const [loading, setLoading] = useState(false);
+  const { id } = useParams();
+  const location = useLocation();
+  const backToPrevLocation = location?.state?.from ?? '/';
+  useEffect(() => {
+    const findCountry = async () => {
+      setLoading(true);
+      try {
+        const fetchCountryResponse = await fetchCountry(id);
+        setFindCountry(fetchCountryResponse);
+      } catch (error) {
+        console.log(error);
+      } finally {
+        setLoading(false);
+      }
+    };
+    findCountry();
+  }, [id]);
+  console.log(findCountry);
   return (
     <Section>
       <Container>
-        <h2>Country</h2>
+        {loading && <Loader />}
+        <CountryInfo country={findCountry} />
+        <GoBackBtn path={backToPrevLocation}>Go Back</GoBackBtn>
       </Container>
     </Section>
   );
