b50bedcf2b6086ed3fe94d0c0aa14bcc5ed87d4c
Robertw8 <oniedgaf@gmail.com>
2023-10-06 20:51:04 +0300

    1

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,38 @@
+import { useState, useEffect } from 'react';
+
 import { Container, CountryList, Heading, Loader, Section } from 'components';
 
+import { getCountries } from 'service/country-service';
+
 export const Home = () => {
+  const [countries, setCountries] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+
+  useEffect(() => {
+    const fetchCountries = async () => {
+      setIsLoading(true);
+      try {
+        const result = await getCountries();
+        setCountries(result);
+      } catch (error) {
+        console.warn(error);
+      } finally {
+        setIsLoading(false);
+      }
+    };
+
+    fetchCountries();
+  }, []);
   return (
     <Section>
       <Container>
         <h2>Home</h2>
+        {countries.length ? (
+          <CountryList countries={countries} />
+        ) : (
+          <Heading>No countries found.</Heading>
+        )}
+        {isLoading && <Loader />}
       </Container>
     </Section>
   );
